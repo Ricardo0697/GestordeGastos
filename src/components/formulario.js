e452ed0d6a6aeafaf423bca42b80c1733015d42d
Ricardo0697 <37809835+Ricardo0697@users.noreply.github.com>
2022-12-13 11:34:39 -0600

    Se crean carpetas para acomodar los componentes

--- src/components/formulario.js
+++ src/components/formulario.js
@@ -1,22 +1,27 @@
-import './App.css';
+import '../App.css';
 import Button from 'react-bootstrap/Button';
+// import Button from 'semantic-ui-react';
+
 
 function formulario(){
+    const setInvoice = () =>{
+        alert('Hola');
+    }
 return(
     <div className='App'>
         <h1>Formulario de Ingreso</h1>
         <form action='/createAdd' method='POST'>
             <div className='col-md-12'>
-                <input placeholder="Actividad">
+                <input placeholder="Actividad" name='actividad'>
 
                 </input>
             </div>
             <div className='col-md-12'>
                 
-            <input placeholder='Entrada' type="number"></input>
+            <input placeholder='Entrada' name='costo' type="number"></input>
             </div>
             <div className='col-md-12'>
-                <Button variant="outline-success" >Agregar</Button>
+                <Button variant="outline-success" onClick={()=> setInvoice()} >Agregar</Button>
             </div>
         </form>
     </div>
