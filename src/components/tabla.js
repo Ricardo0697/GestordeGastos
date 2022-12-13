ea390b9df3e146f56f861cec6e6ef8418310e363
Ricardo0697 <37809835+Ricardo0697@users.noreply.github.com>
2022-12-13 15:14:17 -0600

    Update tabla.js

--- src/components/tabla.js
+++ src/components/tabla.js
@@ -1,8 +1,15 @@
 import '../App.css';
 export default function Tabla({setInvoice}){
+    const datas = () =>{
+        let array = [];
+        console.log(array);
+        array.push(setInvoice);
+        console.log(array);
+        
+    };
     return (
         <div>
-            {setInvoice}
+            {datas}
         </div>
     );
 }
